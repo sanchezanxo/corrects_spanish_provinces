fe0a9c7b4da2ffcfb1155e7522095dd5d0ea3bd7
Anxo Sánchez <57776875+sanchezanxo@users.noreply.github.com>
2023-08-28 16:30:31 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,23 @@
+=== Create random posts ===
+Tags: place names, toponym, Spanish provinces
+Tested up to: 6.2.2
+Stable tag: 1.0.0
+License: GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+This plugin corrects the names of the Spanish provinces that have been incorrectly translated into Spanish, and restores their original toponym.
+
+== Description ==
+This plugin corrects the names of the Spanish provinces that have been incorrectly translated into Spanish, and restores their original toponym.
+
+La Coruña -> A Coruña
+Orense -> Ourense
+Lérida -> Lleida
+Gerona -> Girona
+
+== Installation ==
+Simply download and activate it. Nothing else is needed.
+
+== Changelog ==
+1.0.0
+Initial release
---
+++ corrects_spanish_provinces.php
@@ -0,0 +1,37 @@
+<?php
+/*
+* Plugin Name: Correct place-names of the provinces of Spain in WooCommerce
+* Plugin URI: https://github.com/sanchezanxo/corrects_spanish_provinces
+* Description: This plugin corrects the names of the Spanish provinces that have been incorrectly translated into Spanish, and restores their original toponym.
+* Version: 1.0.0
+* Author: Anxo Sánchez
+* Author URI: https://anxosanchez.com
+* Text Domain: corrects_spanish_provinces
+* License: GPL3
+* License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+
+function corrects_spanish_provinces($translated_text, $text, $domain) {
+    if ($domain === 'woocommerce') {
+        switch ($translated_text) {
+            case 'La Coruña':
+                $translated_text = 'A Coruña';
+                break;
+            case 'Orense':
+                $translated_text = 'Ourense';
+                break;
+            case 'Lérida':
+                $translated_text = 'Lleida';
+                break;
+            case 'Gerona':
+                $translated_text = 'Girona';
+                break;				
+        }
+    }
+    return $translated_text;
+}
+
+add_filter('gettext', 'corrects_spanish_provinces', 20, 3);
+
+ ?>
